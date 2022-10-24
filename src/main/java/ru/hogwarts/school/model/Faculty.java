23ea42c2dd4bde4c0bd6dcde2dd721407cbbb9eb
Ainur <ainurforex@mail.ru>
2022-10-24 19:10:42 +0500

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -0,0 +1,37 @@
+package ru.hogwarts.school.model;
+
+public class Faculty {
+    private long id;
+    private String name;
+    private String color;
+
+    public Faculty(long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
