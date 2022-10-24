80b9912525a58b05b3ad87c626cfbd333f223ec0
Ainur <ainurforex@mail.ru>
2022-10-24 22:08:23 +0500

    1

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,63 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.FacultyService;
+
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "facultys")
+public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @GetMapping(path = " {id}")
+    public ResponseEntity<Faculty> getFacultyInfo(@PathVariable Long id) {
+        Faculty faculty = facultyService.findFaculty(id);
+        if (faculty == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(faculty);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Faculty>> getAllFacultys() {
+        return ResponseEntity.ok(facultyService.getAllFacultys());
+    }
+
+    @GetMapping(path = "byColor/{color}")
+    public ResponseEntity<Collection<Faculty>> getStudentsByAge(@PathVariable String color) {
+        return ResponseEntity.ok(facultyService.getFacultyByColor(color));
+    }
+    @PostMapping
+    public Faculty creatFaculty(@RequestBody Faculty faculty) {
+        return facultyService.creatFaculty(faculty);
+    }
+
+    @PutMapping
+    public ResponseEntity<Faculty> editStudent(@RequestBody Faculty faculty) {
+        Faculty foundFaculty = facultyService.editFaculty(faculty);
+        if (foundFaculty == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(foundFaculty);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Faculty> deleteFaculty(@PathVariable Long id) {
+        Faculty deleteFaculty = facultyService.deleteFaculty(id);
+        if (deleteFaculty == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(deleteFaculty);
+    }
+}
--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,14 +1,38 @@
 package ru.hogwarts.school.model;
 
+import java.util.Objects;
+
 public class Student {
     private long id;
     private String name;
-    private String color;
+    private int age;
 
-    public Student(long id, String name, String color) {
+    public Student(Long id, String name, int age) {
         this.id = id;
         this.name = name;
-        this.color = color;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+        return getAge() == student.getAge() && Objects.equals(getId(), student.getId()) && Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getAge());
     }
 
     public long getId() {
@@ -19,8 +43,8 @@ public class Student {
         return name;
     }
 
-    public String getColor() {
-        return color;
+    public int getAge() {
+        return age;
     }
 
     public void setId(long id) {
@@ -31,7 +55,10 @@ public class Student {
         this.name = name;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setAge(int age) {
+        this.age = age;
     }
+
+
+
 }
